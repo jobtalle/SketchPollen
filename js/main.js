89d2a18bcea027626d12db490f7ba0e3e4df54ca
jobtalle <jobtalle@hotmail.com>
2019-08-08 12:09:57 +0200

    Created plants with growing stalks

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
+const PLANT_TIME_MIN = 1;
+const PLANT_TIME_MAX = 4;
+const PLANT_CEILING = 0.5;
 const TIME_STEP_MAX = 0.2;
 
 const wrapper = document.getElementById("wrapper");
 const canvas = document.getElementById("renderer");
+const plants = [];
+let plantTimer = 0;
 let lastDate = new Date();
 
 const resize = () => {
@@ -9,15 +14,33 @@ const resize = () => {
     canvas.height = wrapper.offsetHeight;
 };
 
+const spawnPlant = center => {
+    plants.push(new Plant(
+        center ? canvas.width * 0.5 : Math.random() * canvas.width,
+        canvas.height,
+        canvas.height * PLANT_CEILING));
+};
+
 const update = timeStep => {
     if (timeStep > TIME_STEP_MAX)
         timeStep = TIME_STEP_MAX;
+
+    if ((plantTimer -= timeStep) < 0) {
+        plantTimer = PLANT_TIME_MIN + (PLANT_TIME_MAX - PLANT_TIME_MIN) * Math.random();
+
+        spawnPlant(false);
+    }
+
+    for (let i = plants.length; i-- > 0;)
+        if (plants[i].update(timeStep))
+            plants.splice(i, 1);
 
     const context = canvas.getContext("2d");
 
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-
+    for (const plant of plants)
+        plant.draw(context);
 };
 
 const loopFunction = () => {
@@ -33,3 +56,5 @@ window.onresize = resize;
 
 resize();
 requestAnimationFrame(loopFunction);
+
+spawnPlant(true);
--- js/plant.js
+++ js/plant.js
@@ -1,3 +1,95 @@
-const Plant = function() {
+const Stalk = function(x, y) {
+    const Point = function(x, y) {
+        this.x = x;
+        this.y = y;
+    };
 
+    const points = [new Point(x, y), new Point(x, y)];
+
+    this.getX = () => x;
+    this.getY = () => y;
+
+    this.extrude = (x, y) => {
+        points[points.length - 1].x = x;
+        points[points.length - 1].y = y;
+
+        const dx = x - points[points.length - 2].x;
+        const dy = y - points[points.length - 2].y;
+
+        if (Math.sqrt(dx * dx + dy * dy) > Stalk.RESOLUTION)
+            points.push(new Point(x, y));
+    };
+
+    this.update = timeStep => {
+
+    };
+
+    this.draw = context => {
+        context.strokeStyle = "black";
+        context.beginPath();
+        context.moveTo(points[0].x, points[0].y);
+
+        for (let i = 1; i < points.length; ++i)
+            context.lineTo(points[i].x, points[i].y);
+
+        context.stroke();
+    };
 };
+
+Stalk.RESOLUTION = 64;
+
+const Phytomer = function(stalk) {
+    const noise = cubicNoiseConfig(Math.random());
+    let direction;
+    let x = stalk.getX();
+    let y = stalk.getY();
+
+    const sampleDirection = () => {
+        direction = cubicNoiseSample2(
+            noise,
+            x * Phytomer.NOISE_SCALE,
+            y * Phytomer.NOISE_SCALE) * Math.PI * 2;
+    };
+
+    this.update = (timeStep, growthSpeed) => {
+        x += Math.cos(direction) * growthSpeed * timeStep;
+        y += Math.sin(direction) * growthSpeed * timeStep;
+
+        stalk.extrude(x, y);
+
+        sampleDirection();
+
+        return false;
+    };
+
+    sampleDirection();
+};
+
+Phytomer.NOISE_SCALE = 0.01;
+
+const Plant = function(x, floor, ceiling) {
+    const growthSpeed = Plant.GROWTH_SPEED_MIN + (Plant.GROWTH_SPEED_MAX - Plant.GROWTH_SPEED_MIN) * Math.random();
+    const stalks = [new Stalk(x, floor)];
+    const phytomers = [new Phytomer(stalks[0])];
+
+    this.draw = context => {
+        for (const stalk of stalks)
+            stalk.draw(context);
+
+        context.fillStyle = "blue";
+        context.beginPath();
+        context.arc(x, floor, 12, 0, Math.PI * 2);
+        context.fill();
+    };
+
+    this.update = timeStep => {
+        for (let i = phytomers.length; i-- > 0;)
+            if (phytomers[i].update(timeStep, growthSpeed))
+                phytomers.splice(i, 1);
+
+        return false;
+    };
+};
+
+Plant.GROWTH_SPEED_MIN = 16;
+Plant.GROWTH_SPEED_MAX = 32;
